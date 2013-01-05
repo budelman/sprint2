aedfe11137fe0fff6781f0f9594fd24c2d3d4c2d
Mat Budelman <mattbudelman@gmail.com>
2013-01-05 16:47:18 -0500

    checking server status

--- js/addressbook.js
+++ js/addressbook.js
@@ -1,11 +1,17 @@
 // javascript Ajax Request
 var request = getHTTPObject();
 
+request.onreadystatechange = function () {
+	
+	// check if the request is ready and that it was successful
+	if( request.redystate === 4 && request.status === 200 ) {
+		
+		// do something
+	}
+}
+
 /* Get all the information ready to go */
 request.open("GET", "data/contacts.json", true);
 
 /* initiate actual call */
 request.send(null);
-
-/* OR - initiate the call with some data */
-request.send("hello data");
