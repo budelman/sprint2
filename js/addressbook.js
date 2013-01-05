c7043f4c006eb4fee04da9a1e20c44168fee5f36
Mat Budelman <mattbudelman@gmail.com>
2013-01-05 16:39:36 -0500

    sending data

--- js/addressbook.js
+++ js/addressbook.js
@@ -2,6 +2,10 @@
 var request = getHTTPObject();
 
 /* Get all the information ready to go */
-
 request.open("GET", "data/contacts.json", true);
 
+/* initiate actual call */
+request.send(null);
+
+/* OR - initiate the call with some data */
+request.send("hello data");
